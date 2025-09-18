e36cb567c23a2959dbdfcaca867ed57313030eef
Rodry-x <166262311+Rodry-x@users.noreply.github.com>
2025-09-18 14:41:11 -0500

    Añadiendo FXML
    Añadiendo paginas de:
    - Gestor de biblioteca (como una estantería virtual)
    - Panel de bienvenida

---
+++ Proyecto-EF/src/main/java/utilities/Path.java
@@ -0,0 +1,6 @@
+package utilities;
+
+public class Path {
+    public static final String PANEL_PRINCIPAL_FXML = "/PanelDeInicio.fxml";
+    public static final String PANEL_LIBRERIA_FXML = "/CrudLibreria.fxml";
+}
