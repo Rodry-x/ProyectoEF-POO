9f2a7826e2f4bcf773f34745ce839f32b96d4a0a
Rodry-x <166262311+Rodry-x@users.noreply.github.com>
2025-09-18 23:19:04 -0500

    Se añadio SceneController
    Se agrego la funcionalidad de cambiar entre pestañas:
    - Paths
    - SceneController

--- Proyecto-EF/src/main/java/controller/CrudLibrosController.java
+++ Proyecto-EF/src/main/java/controller/CrudLibrosController.java
@@ -2,13 +2,22 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-public class BienvenidaLibreriaVirtualController {
+public class CrudLibrosController {
     //Usando la clase SceneController para cambiar de escenas
     private final SceneController sceneController = new SceneController();
+
+    @FXML
+    private Label lblNombreDelLibro;
+
+    public void setNombreDelLibro(String nombre) {
+        lblNombreDelLibro.setText(nombre);
+    }
 
     @FXML
     void cambiarAEstanteria(ActionEvent event) {
         sceneController.cambiarAEstanteriaVirtual(event);
     }
+
 }
