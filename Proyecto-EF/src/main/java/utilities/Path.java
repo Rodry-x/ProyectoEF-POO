e35ca991e753770474d8c8431219b84a0da86a5e
Rodry-x <166262311+Rodry-x@users.noreply.github.com>
2025-10-02 17:00:05 -0500

    Delete Proyecto-EF directory

--- Proyecto-EF/src/main/java/utilities/Path.java
+++
@@ -1,6 +0,0 @@
-package utilities;
-
-public class Path {
-    public static final String PANEL_PRINCIPAL_FXML = "/PanelDeInicio.fxml";
-    public static final String PANEL_LIBRERIA_FXML = "/CrudLibreria.fxml";
-}
--- Proyecto-EF/src/main/java/utilities/Paths.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class Paths {
-    public static final String PANEL_PRINCIPAL_FXML = "/BienvenidaLibreriaVirtual.fxml";
-    public static final String PANEL_ESTANTERIA_FXML = "/EstanteriaVirtual.fxml";
-    public static final String PANEL_LIBROS_FXML = "/CrudLibros.fxml";
-}
